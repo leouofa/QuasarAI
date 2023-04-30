ce4e29de5cb44d2c1290a7d8fa39c02c81358df6
Leonid Medovyy <leonid@storypro.io>
2023-04-29 20:04:40 -0700

    added rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'capybara/rspec'
+require 'rack_session_access/capybara'
+require 'byebug'
+require 'knapsack_pro'
+
+KnapsackPro::Adapters::RSpecAdapter.bind
+
+`rake blueprints:update`
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.include Capybara::DSL
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.default_formatter = 'doc'
+  config.order = :random
+end
