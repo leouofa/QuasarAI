d43df43b736351c57e1c413cc0b5307d5a05bcbc
Leonid Medovyy <leonid@storypro.io>
2023-05-02 17:28:37 -0700

    added ability to injest feeds

--- app/jobs/ingest_feed_job.rb
+++ app/jobs/ingest_feed_job.rb
@@ -3,8 +3,24 @@ class IngestFeedJob < ApplicationJob
 
   def perform(sub_topic:)
     @sub_topic = sub_topic
-    @feed = Feed.new(sub_topic:)
 
-    # Do something later
+    if @sub_topic.feeds.blank?
+      ingest_feed
+      return
+    end
+
+    formatted_today_date = Time.now.utc.to_date.strftime("%m/%d/%Y")
+    @last_feed = @sub_topic.feeds.last
+
+    unless @last_feed.created_at.strftime("%m/%d/%Y") == formatted_today_date
+      puts 'its blank', formatted_today_date, @last_feed.created_at.strftime("%m/%d/%Y")
+      ingest_feed
+    end
+  end
+
+  def ingest_feed
+    puts 'Ingesting feedly feed'
+    feed = Feedly.get_contents(@sub_topic.stream_id)
+    Feed.create(sub_topic: @sub_topic, payload: feed)
   end
 end
