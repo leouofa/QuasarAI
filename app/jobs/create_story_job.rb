caeebd75b10064cc17ad5878365de645e60d4a80
Leonid Medovyy <leonid@storypro.io>
2023-05-04 09:05:30 -0700

    cleaning up

--- app/jobs/create_story_job.rb
+++ app/jobs/create_story_job.rb
@@ -7,27 +7,29 @@ class CreateStoryJob < ApplicationJob
 
     feeds_with_subtopic = Feed.where(sub_topic: sub_topic)
     feeds_with_subtopic_ids = feeds_with_subtopic.pluck(:id)
-    # ap feeds_with_subtopic_ids
-    #
+
     feed_items = FeedItem.where(processed: false, feed_id: feeds_with_subtopic_ids)
     feed_items_ids = feed_items.pluck(:id)
+
+
+    # ALTERNATVE IMPLEMENTATION OF TAG FREQUENCY
+    # freq_array = {}
     #
-    freq_array = {}
-
-    taggings = Tagging.joins(:tag).where(feed_item_id: feed_items_ids)
-    taggings.each do |tagging|
-      freq_array[tagging.tag.name] = if freq_array[tagging.tag.name].blank?
-                                       1
-                                     else
-                                       freq_array[tagging.tag.name] + 1
-                                     end
-    end
-
-    sorted_tag_frequency = freq_array.sort { |a,b| b.last <=> a.last }
+    # taggings = Tagging.joins(:tag).where(feed_item_id: feed_items_ids)
+    # taggings.each do |tagging|
+    #   freq_array[tagging.tag.name] = if freq_array[tagging.tag.name].blank?
+    #                                    1
+    #                                  else
+    #                                    freq_array[tagging.tag.name] + 1
+    #                                  end
+    # end
+    # sorted_tag_frequency = freq_array.sort { |a,b| b.last <=> a.last }
 
 
-    # tag_frequency = Tag.joins(taggings: { feed_item: :feed }).where(feed_items: { processed: false }, feeds: { sub_topic_id: sub_topic.id }).group(:name).count(:id)
-    # sorted_tag_frequency = tag_frequency.sort { |a,b| b.last <=> a.last }
+    tag_frequency = Tag.joins(taggings: { feed_item: :feed })
+                       .where(feed_items: { processed: false }, feeds: { sub_topic_id: sub_topic.id })
+                       .group(:name).count(:id)
+    sorted_tag_frequency = tag_frequency.sort { |a,b| b.last <=> a.last }
 
     # byebug
 
@@ -42,7 +44,10 @@ class CreateStoryJob < ApplicationJob
 
 
 
-    available_feed_items = FeedItem.joins(taggings: :tag).where(id: feed_items_ids, tags: { name: story_tag_name }, feed_items: { processed: false })
+    available_feed_items = FeedItem.joins(taggings: :tag)
+                                   .where(id: feed_items_ids,
+                                          tags: { name: story_tag_name },
+                                          feed_items: { processed: false })
 
 
     story = Story.new
@@ -55,14 +60,6 @@ class CreateStoryJob < ApplicationJob
       available_feed_item.update(processed: true)
     end
 
-    ap sorted_tag_frequency
     ap "Creating story for subtopic: #{sub_topic.name}, with tag name: #{story_tag_name} and frequency: #{story_tag_frequency}."
-
-
-
-    # tag_frequency = Tag.joins(taggings: {feed_item: :feed}).where(feeds: {id: sub_topic.id}).select("tags.name").group(:name).count
-    #  sub_topic.feeds.last.feed_items.last.tags
-    #  Tag.joins(taggings: {feed_item: :feed}).where(feeds: {id: sub_topic.id})
-    # byebug
   end
 end
