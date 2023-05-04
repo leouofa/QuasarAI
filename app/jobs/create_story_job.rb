1b5d08e50c3bb98753604663a502e8d18543868a
Leonid Medovyy <leonid@storypro.io>
2023-05-04 08:54:53 -0700

    refactorign the story job

--- app/jobs/create_story_job.rb
+++ app/jobs/create_story_job.rb
@@ -1,19 +1,40 @@
 class CreateStoryJob < ApplicationJob
   queue_as :default
-  MINIMUM_NUMBER_OF_TAGS_FOR_STORY = 3
+  MINIMUM_NUMBER_OF_TAGS_FOR_STORY = 2
 
   def perform(sub_topic:)
     @sub_topic = sub_topic
 
-    tag_frequency = Tag.joins(taggings: { feed_item: :feed })
-                       .where(feed_items: { processed: false }, feeds: { id: sub_topic.id })
-                       .group(:name)
-                       .count(:id)
-    sorted_tag_frequency = tag_frequency.sort { |a,b| b.last <=> a.last }
+    feeds_with_subtopic = Feed.where(sub_topic: sub_topic)
+    feeds_with_subtopic_ids = feeds_with_subtopic.pluck(:id)
+    # ap feeds_with_subtopic_ids
+    #
+    feed_items = FeedItem.where(processed: false, feed_id: feeds_with_subtopic_ids)
+    feed_items_ids = feed_items.pluck(:id)
+    #
+    freq_array = {}
+
+    taggings = Tagging.joins(:tag).where(feed_item_id: feed_items_ids)
+    taggings.each do |tagging|
+      freq_array[tagging.tag.name] = if freq_array[tagging.tag.name].blank?
+                                       1
+                                     else
+                                       freq_array[tagging.tag.name] + 1
+                                     end
+    end
+
+    sorted_tag_frequency = freq_array.sort { |a,b| b.last <=> a.last }
+
+
+    # tag_frequency = Tag.joins(taggings: { feed_item: :feed }).where(feed_items: { processed: false }, feeds: { sub_topic_id: sub_topic.id }).group(:name).count(:id)
+    # sorted_tag_frequency = tag_frequency.sort { |a,b| b.last <=> a.last }
+
+    # byebug
 
     # too few tags don't do anything
     return if sorted_tag_frequency.size < 2
     return if sorted_tag_frequency[1][1] < MINIMUM_NUMBER_OF_TAGS_FOR_STORY
+
 
     story_tag_name = sorted_tag_frequency[1][0]
     story_tag_frequency = sorted_tag_frequency[1][1]
@@ -21,15 +42,17 @@ class CreateStoryJob < ApplicationJob
 
 
 
-    feed_items = FeedItem.joins(taggings: :tag).where(tags: { name: story_tag_name }, feed_items: { processed: false })
+    available_feed_items = FeedItem.joins(taggings: :tag).where(id: feed_items_ids, tags: { name: story_tag_name }, feed_items: { processed: false })
+
 
     story = Story.new
     story.tag = tag
     story.save
 
-    feed_items.sample(MINIMUM_NUMBER_OF_TAGS_FOR_STORY).each do |feed_item|
-      story.feed_items << feed_item
-      feed_item.update(processed: true)
+
+    available_feed_items.sample(MINIMUM_NUMBER_OF_TAGS_FOR_STORY).each do |available_feed_item|
+      story.feed_items << available_feed_item
+      available_feed_item.update(processed: true)
     end
 
     ap sorted_tag_frequency
