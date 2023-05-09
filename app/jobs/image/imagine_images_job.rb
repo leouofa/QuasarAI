6899ac10b1bbb7e29d9208ef5990efb821ce0dab
Leonid Medovyy <leonid@storypro.io>
2023-05-09 07:03:25 -0700

    setting up the midjourney

--- app/jobs/image/imagine_images_job.rb
+++ app/jobs/image/imagine_images_job.rb
@@ -6,7 +6,16 @@ class Image::ImagineImagesJob < ApplicationJob
     images_to_process = Image.where(invalid_prompt: false).where.not(id: images_ids)
     images_to_process.each do |image|
       Images::ImagineImageJob.perform_now(image:)
-      sleep(120)
+
+      pending_imaginations = Imagination.where(status: :pending).count
+
+      if pending_imaginations > 3
+        sleep(300)
+      elsif pending_imaginations > 2
+        sleep(180)
+      else
+        sleep(120)
+      end
     end
   end
 end
