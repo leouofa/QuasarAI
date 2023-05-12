2f47abce487eb3a5c7698aea580bf8c8594651cc
Leonid Medovyy <leonid@storypro.io>
2023-05-12 06:59:19 -0700

    cleaning up

--- app/jobs/images/create_image_ideas_from_stories_job.rb
+++ app/jobs/images/create_image_ideas_from_stories_job.rb
@@ -3,10 +3,6 @@ module Images
     queue_as :default
 
     def perform(*_args)
-      # TODO ensure that changing this query did not break anything. Specifically the `processed: true` part.
-      # stories_with_no_images = Story.where.not(id: Image.select(:story_id).where(processed: true))
-      #                               .where(invalid_images: false, invalid_json: false)
-
       stories_without_images = Story.where.not(id: Image.select(:story_id))
                      .where(invalid_images: false, invalid_json: false)
 
