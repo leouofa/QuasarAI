8129f40fe38dd229b91b516ba4c78de680827bb0
Leonid Medovyy <leonid@storypro.io>
2023-05-13 07:54:12 -0700

    updating workflow

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_13_012753) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_13_145332) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "assignments", force: :cascade do |t|
