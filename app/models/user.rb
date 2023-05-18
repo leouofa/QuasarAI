86f029c99d24f83f948a1de9d2ddccf32983eb25
Leonid Medovyy <leonid@storypro.io>
2023-05-17 19:18:39 -0700

    disabled confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :lockable, :trackable, :confirmable
+         :recoverable, :rememberable, :validatable, :lockable, :trackable
 
   def make_admin
     update(admin: true)
