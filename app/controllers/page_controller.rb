0c394b3405e42b2ab2e8971eb84d63727a046c3d
Leonid Medovyy <leonid@storypro.io>
2023-07-24 09:27:15 -0700

    updated the dashboard

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,5 +2,11 @@ class PageController < ApplicationController
   include RestrictedAccess
   def index
     @topics = Topic.with_active_subtopic.order(:name)
+    @feeds_count = Feed.count
+    @feed_items_count = FeedItem.count
+    @stories_count = Story.count
+    @discussions_count = Discussion.count
+    @imaginations_count = Imagination.count
+    @tweets_count = Tweet.count
   end
 end
