dfb6525ea5547b5dca56beb4f9480bee74e4ca7d
Leonid Medovyy <leonid@storypro.io>
2023-07-25 08:56:23 -0700

    added the ability to chage the pubishig window

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -11,6 +11,16 @@ class SettingsController < ApplicationController
     return unless current_user.admin?
 
     @settings = Setting.instance
+
+    start_time = settings_params[:publish_start_time]
+    end_time = settings_params[:publish_end_time]
+
+    if !valid_time_format?(start_time) || !valid_time_format?(end_time)
+      @settings.errors.add(:base, 'Both `start` and `end` time should be HH:MM format.')
+      render :edit
+      return
+    end
+
     if @settings.update(settings_params)
       UpdateSettingsJob.perform_now
 
@@ -24,5 +34,10 @@ class SettingsController < ApplicationController
 
   def settings_params
     params.require(:setting).permit(:topics, :prompts, :tunings, :publish_start_time, :publish_end_time)
+  end
+
+  def valid_time_format?(time)
+    time_format = /\A([01]\d|2[0-3]):([0-5]\d)\z/
+    time_format.match?(time)
   end
 end
