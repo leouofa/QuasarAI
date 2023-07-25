59c1630ee906dba314bf0f777d1f15592ca1042f
Leonid Medovyy <leonid@storypro.io>
2023-07-25 08:02:22 -0700

    added tweet publishing window constraint

--- app/jobs/tweets/publish_job.rb
+++ app/jobs/tweets/publish_job.rb
@@ -3,6 +3,9 @@ class Tweets::PublishJob < ApplicationJob
   MAX_TWEETS_PUBLISHED_AT_A_TIME = 1
 
   def perform(*args)
+    settings = Setting.instance
+    return unless settings.within_publish_window?
+
     Discussion.ready_to_upload_tweets.sample(MAX_TWEETS_PUBLISHED_AT_A_TIME).each do |discussion|
       Tweets::PublishTweetJob.perform_now(discussion:)
     end
