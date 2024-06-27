68ddb9a08440828db788f79e878c720ccedde875
Leonid Medovyy <leonid@macron1.io>
2024-06-27 12:34:51 -0700

    installing neihbor and pg_vector

---
+++ db/migrate/20240627193403_install_neighbor_vector.rb
@@ -0,0 +1,5 @@
+class InstallNeighborVector < ActiveRecord::Migration[7.0]
+  def change
+    enable_extension "vector"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_26_211214) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_27_193403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
+  enable_extension "vector"
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
