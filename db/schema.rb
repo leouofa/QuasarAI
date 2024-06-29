fdf0058acd34ade4c9845c1399a981b504d92dd9
Leonid Medovyy <leonid@macron1.io>
2024-06-29 16:04:00 -0700

    adding the emedding field to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_29_161427) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_29_230258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_29_161427) do
     t.datetime "rewritten_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.vector "embedding", limit: 768
     t.index ["pillar_column_id"], name: "index_articles_on_pillar_column_id"
   end
 
