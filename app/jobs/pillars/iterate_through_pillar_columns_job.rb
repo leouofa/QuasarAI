bec94771a8b6772db8f36bb156165824fe5c9dcb
Leonid Medovyy <leonid@macron1.io>
2024-07-21 18:10:21 -0700

    updating pillar column logic

--- app/jobs/pillars/iterate_through_pillar_columns_job.rb
+++ app/jobs/pillars/iterate_through_pillar_columns_job.rb
@@ -2,7 +2,7 @@ class Pillars::IterateThroughPillarColumnsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    PillarColumn.without_topics.each do |pillar_column|
+    PillarColumn.without_topics.shuffle.each do |pillar_column|
       Pillars::CreatePillarColumnTopicsJob.perform_now(pillar_column:)
     end
   end
